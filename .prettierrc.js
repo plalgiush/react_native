029fd9bfeacda9d8610b313e8d6300f41eb13730
plalgiush <ilyasushko@yandex.ru>
2024-01-15 22:18:34 +0300

    change prettier config: - bracketSpacing; - printWidth;

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
   tabWidth: 2,
   useTabs: false,
   semi: true,
-  printWidth: 80,
-};
+  printWidth: 120
+}
